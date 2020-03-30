4f45f9367e94b1fb6e3288be449a39d16b9916dc
Tianyun Zhang (VMWare) <doowzs@qq.com>
2020-03-30 15:09:15 +0800

    update output of error S

--- Code/semantics.c
+++ Code/semantics.c
@@ -6,26 +6,26 @@ extern bool hasErrorS;
 extern STNode *stroot;
 
 const STError SETable[] = {
-  {  0, false, "Pseudo error", "" },
-  {  1,  true, "Use of undefined variable", "" },
-  {  2,  true, "Use of undefined function", "" },
-  {  3,  true, "Duplicated name", "of variable" },
-  {  4,  true, "Redefinition of function", "" },
-  {  5, false, "Type mismatched for assignment", "" },
-  {  6, false, "Cannot assign to rvalue", "" },
-  {  7, false, "Type mismatched for operands", "" },
-  {  8, false, "Type mismatched for return" },
-  {  9,  true, "Arguments mismatched for function", "" },
-  { 10,  true, "Array access on non-array variable", "" },
-  { 11,  true, "Function call on non-function variable", "" },
-  { 12, false, "Non integer offset of an array", "" },
-  { 13,  true, "Field access on non-struct variable", "" },
-  { 14,  true, "Access to undefined field", "in struct" },
-  { 15,  true, "Redefinition or initialization of field", "in struct" },
-  { 16,  true, "Duplicated name", "of struct" },
-  { 17,  true, "Use of undefined struct", "" },
-  { 18,  true, "Function", "declared but not declared" },
-  { 19,  true, "Inconsistent declaration of function", "" },
+  {  0, false, "Pseudo error ", "" },
+  {  1,  true, "Use of undefined variable ", "" },
+  {  2,  true, "Use of undefined function ", "" },
+  {  3,  true, "Duplicated name ", " of variables" },
+  {  4,  true, "Redefinition of function ", "" },
+  {  5, false, "Type mismatched for assignment ", "" },
+  {  6, false, "Cannot assign to rvalue ", "" },
+  {  7, false, "Type mismatched for operands ", "" },
+  {  8, false, "Type mismatched for return " },
+  {  9,  true, "Arguments mismatched for function ", "" },
+  { 10,  true, "Array access on non-array variable ", "" },
+  { 11,  true, "Function call on non-function variable ", "" },
+  { 12, false, "Non integer offset of an array ", "" },
+  { 13,  true, "Field access on non-struct variable ", "" },
+  { 14,  true, "Access to undefined field ", " in struct" },
+  { 15,  true, "Redefinition or initialization of field ", " in struct" },
+  { 16,  true, "Duplicated name ", " of struct" },
+  { 17,  true, "Use of undefined struct ", "" },
+  { 18,  true, "Function ", " declared but not declared" },
+  { 19,  true, "Inconsistent declaration of function ", "" },
 };
 
 STStack *baseStack = NULL;
@@ -102,7 +102,7 @@ void throwErrorS(int id, STNode *node) {
   hasErrorS = true;
   fprintf(stderr, "Error type %d at Line %d: ", id, node->line);
   if (SETable[id].showId) {
-    fprintf(stderr, "%s \"%s\" %s", SETable[id].message1, node->sval, SETable[id].message2);
+    fprintf(stderr, "%s\"%s\"%s", SETable[id].message1, node->sval, SETable[id].message2);
   } else {
     fprintf(stderr, "%s", SETable[id].message1);
   }

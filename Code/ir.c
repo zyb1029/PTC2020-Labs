744446a5b57e5a9d9b939efecbdadc6a18daafc3
Tianyun Zhang (Code) <doowzs@qq.com>
2020-05-04 07:38:42 +0000

    update comments

--- Code/ir.c
+++ Code/ir.c
@@ -204,7 +204,7 @@ IRCodePair IRTranslateExp(STNode *exp, IROperand place, bool deref) {
           if (place.kind != IR_OP_NULL) {
             IRCode *code2 = IRNewCode(IR_CODE_ASSIGN);
             code2->assign.left = place;
-            code2->assign.right = t1;
+            code2->assign.right = t1;  // var may be an address, use t1 instead
             pair.list = IRAppendCode(pair.list, code2);
           }
           return pair;
